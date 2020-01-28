8dae3a33e7b442c6e62b17fe6a845b0bd1e5e8d4
Jsoriano17 <52586265+Jsoriano17@users.noreply.github.com>
2020-01-27 19:24:15 -0700

    added a responsive mobile design

--- port.js
+++ port.js
@@ -1,1 +1,17 @@
 var scroll = new SmoothScroll('a[href*="#"]');
+//nav animation 
+$(document).ready(function () {
+
+    $('.first-button').on('click', function () {
+  
+      $('.animated-icon1').toggleClass('open');
+    });
+    $('.second-button').on('click', function () {
+  
+      $('.animated-icon2').toggleClass('open');
+    });
+    $('.third-button').on('click', function () {
+  
+      $('.animated-icon3').toggleClass('open');
+    });
+  });
